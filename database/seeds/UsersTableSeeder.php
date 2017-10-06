7ae4aef1b4396142b049ec57f2ffe813c4776682
JorgeRafaelL <jorge.alejo.rafael@gmail.com>
2017-10-06 14:49:36 -0500

    second commit

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,33 +1,30 @@
 <?php
 
-use Illuminate\Database\Seeder;
 use App\User;
+use Illuminate\Database\Seeder;
 
-class UsersTableSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-    	//Admin
-        User::create([
-        	'name' => 'Jorge',
-        	'email' => 'jorge@gmail.com',
-        	'password' => bcrypt('crkk7997'),
-        	'role' => 0	
-        ]);
+class UsersTableSeeder extends Seeder {
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		//Admin
+		User::create([
+			'name' => 'Jorge',
+			'email' => 'jorge@gmail.com',
+			'password' => bcrypt('799797'),
+			'role' => 0,
+		]);
 
+		//Client
+		User::create([
+			'name' => 'Mary',
+			'email' => 'client@gmail.com',
+			'password' => bcrypt('123456'),
+			'role' => 2,
+		]);
 
-        //Client
-        User::create([
-        	'name' => 'Mary',
-        	'email' => 'client@gmail.com',
-        	'password' => bcrypt('123456'),
-        	'role' => 2	
-        ]);
-
-    }
+	}
 }
